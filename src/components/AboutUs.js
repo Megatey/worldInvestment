f9303b113cb76c5d7e695c828aa53f3b4ae471f5
Megatey <megatey97@gmail.com>
2023-06-15 01:11:21 +0100

    dev-Ib commit

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useEffect,useState} from 'react'
 import Other from "./Other";
 import TopBtn from "./TopBtn";
 import SideBar from "./SideBar";
@@ -6,12 +6,32 @@ import AboutPage from "./AboutPage";
 import Heritage from "./Heritage";
 
 const AboutUs = () => {
+
+  const [isVisible, setIsVisible] = useState(false);
+
+  const listenToScroll = () => {
+    let heightToHideFrom = 20;
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+  
+    if (winScroll > heightToHideFrom) {
+      !isVisible && // 
+        setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+  
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+    return () => window.removeEventListener("scroll", listenToScroll);
+  });
   return (
     <div>
    <AboutPage/>
       <Other/>
       <Heritage/>
-      <TopBtn/>
+       {isVisible && <TopBtn />}
       <SideBar/>
      </div>
   );
